920384027aed4b5fe12e7625e55cb109fd86f3b4
Damir Aushenov <sfgodness@gmail.com>
2022-01-13 00:57:49 +0300

    Initial Commit

---
+++ Worde/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Worde
+//
+//  Created by Damir Aushenov on 13/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
